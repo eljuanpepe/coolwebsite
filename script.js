5c2f8f34e207cf933db20f631823c17300d0349b
eljuanpepe <elpepechingon@outlook.com>
2025-08-07 15:51:11 -0600

    code

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-const h1 = document.getElementsByTagName("h1")[0]
-const h2 = document.getElementsByTagName("h2")[0]
-const h3 = document.getElementsByTagName("h3")[0]
+const texts = document.getElementsByTagName("main")[0].children
 
 const sleep = (ms) => new Promise(res => setTimeout(res, ms));
 
 async function play() {
-    h2.style.opacity = "0%"
-    h3.style.opacity = "0%"
-    h1.classList.add("fade")
-    await sleep(3500)
-    h2.classList.add("fade")
-    h2.style.opacity = "100%"
-    await sleep(3500)
-    h3.classList.add("fade")
-    h3.style.opacity = "100%"
+    for (let i = 0; i < texts.length; i++) {
+        const element = texts[i];
+        element.classList.add("fade")
+        await sleep(3500)
+        element.style.opacity = "100%"
+    }
 }
 
 play()
